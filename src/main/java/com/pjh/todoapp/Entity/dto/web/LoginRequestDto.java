0a5468e8fcd1660b69153f7f7d769e423ebd6e80
mankcom7708@gmail.com <mankcom3799@hufs.ac.krecho>
2023-11-12 01:20:15 +0900

    feat #6 login, signup 제약조건 추가

--- src/main/java/com/pjh/todoapp/Entity/dto/web/LoginRequestDto.java
+++ src/main/java/com/pjh/todoapp/Entity/dto/web/LoginRequestDto.java
@@ -1,11 +1,25 @@
 package com.pjh.todoapp.Entity.dto.web;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class LoginRequestDto {
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{4,10}$",
+            message="Username must be a minimum of 4 characters and a maximum of 10 characters," +
+                    " consisting of lowercase letters")
     private String username;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$", message="Password must be a minimum of 8 characters and a maximum of 15 characters," +
+            " consisting of both uppercase and lowercase letters and numbers (0~9).")
     private String password;
+
+    /*
+    - username은  `최소 4자 이상, 10자 이하이며 알파벳 소문자(a~z), 숫자(0~9)`로 구성되어야 한다.
+    - password는  `최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9)`로 구성되어야 한다.
+    */
+
 }
--- src/main/java/com/pjh/todoapp/Entity/dto/web/SignupRequestDto.java
+++ src/main/java/com/pjh/todoapp/Entity/dto/web/SignupRequestDto.java
@@ -1,7 +1,9 @@
 package com.pjh.todoapp.Entity.dto.web;
 
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,12 +11,20 @@ import lombok.Setter;
 @Setter
 public class SignupRequestDto {
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9]{4,10}$")
+    @Column(unique = true)
     private String username;
+
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$")
     private String password;
+
     @Email
     @NotBlank
+    @Column(unique = true)
     private String email;
+
     private boolean admin = false;
+
     private String adminToken = "";
 }
